daebf001fc7d0d33ec77063dd219a632079d9f81
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-13 17:08:00 -0300

    Melhorando o método POST de User

--- src/main/java/com/gabrielyoshino/rest/webservice/restfulwebservices/user/UserResource.java
+++ src/main/java/com/gabrielyoshino/rest/webservice/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.gabrielyoshino.rest.webservice.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,14 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
