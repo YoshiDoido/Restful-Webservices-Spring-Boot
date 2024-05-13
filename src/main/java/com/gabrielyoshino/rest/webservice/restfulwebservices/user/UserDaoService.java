a2126b9d58b10b966f01fff957e9cfd5c12e510e
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-05-13 16:43:51 -0300

    Método POST criado

--- src/main/java/com/gabrielyoshino/rest/webservice/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/gabrielyoshino/rest/webservice/restfulwebservices/user/UserDaoService.java
@@ -5,20 +5,35 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
 
     private static List<User> users = new ArrayList<>();
 
+    // Checa a quantidade de usuários
+    private static Integer usersCount = 0;
+
     static {
-        users.add(new User(1, "Adão", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eva", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Caim", LocalDate.now().minusYears(10).minusMonths(9)));
-        users.add(new User(4, "Abel", LocalDate.now().minusYears(10)));
+        users.add(new User(++usersCount, "Adão", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Eva", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Caim", LocalDate.now().minusYears(10).minusMonths(9)));
+        users.add(new User(++usersCount, "Abel", LocalDate.now().minusYears(10)));
     }
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
