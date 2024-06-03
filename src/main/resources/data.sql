815330c8fde05a6b15f3ed3b1f273be28dda796c
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-06-03 19:38:41 -0300

    Adicionado User Entity e alguns testes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_details (id, birth_date, name) VALUES (10001, current_date(), 'Yoshi');
+INSERT INTO user_details (id, birth_date, name) VALUES (10002, current_date(), 'Mario');
+INSERT INTO user_details (id, birth_date, name) VALUES (10003, current_date(), 'Luigi')
