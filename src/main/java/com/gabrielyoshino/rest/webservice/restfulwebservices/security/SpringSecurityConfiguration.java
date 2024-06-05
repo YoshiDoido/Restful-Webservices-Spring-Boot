b05bf9922903dd57f69b58c134b70e359ac722ab
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-06-05 16:52:33 -0300

    Melhorando a segurança para autenticação básica

---
+++ src/main/java/com/gabrielyoshino/rest/webservice/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.gabrielyoshino.rest.webservice.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
